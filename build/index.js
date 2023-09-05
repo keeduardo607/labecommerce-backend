37ef3dedbf010e5a2b118e4a20a998a94188e1d0
keeduardo607 <keeduardo607@gmail.com>
2023-09-05 16:49:53 -0300

    typescript-i

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+//console.log('Aplicativo INDEX.TS foi iniciado!');
+Object.defineProperty(exports, "__esModule", { value: true });
+const dataBase_1 = require("./dataBase");
+console.log('Usuarios:', dataBase_1.users);
+console.log('Produtos:', dataBase_1.products);
